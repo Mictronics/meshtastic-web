d02dd64252ccf40084996066693ab26b650656fb
Mictronics <github@mictronics.de>
2025-09-12 18:34:31 +0200

    Remove away translation.

--- packages/web/src/pages/Nodes/index.tsx
+++ packages/web/src/pages/Nodes/index.tsx
@@ -183,7 +183,7 @@ const NodesPage = (): JSX.Element => {
                       (node.hopsAway ?? 0 > 1)
                         ? t("unit.hop.plural")
                         : t("unit.hops_one")
-                    } ${t("nodesTable.connectionStatus.away")}`
+                    }`
                 : t("unknown.longName")}
               {node?.viaMqtt === true
                 ? t("nodesTable.connectionStatus.viaMqtt")
